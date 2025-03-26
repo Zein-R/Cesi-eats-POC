ab5ad4b993b66d4120b1145885d846e7991a4d7a
aymen-elo <aymen.elouagouti@gmaile.com>
2025-03-26 14:47:59 +0100

    update entrez votre adresse

--- Cesi-Eats-Front/views/HomeView.vue
+++ Cesi-Eats-Front/views/HomeView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 
+import InputText from "primevue/inputtext";
 </script>
 
 <template>
@@ -8,13 +9,15 @@
     <div class="text-black w-full p-2">
       <h1 class="font-bold">Bienvenue sur <span class="bronze-cesieats">CESI Eats</span></h1>
       <div class="pl-1 opacity-50">Mangez ce que vous voulez (tout en restant en pyjama) !</div>
-      <div class="mt-3 p-2 text-black border-3 rounded-4xl border-orange-500 inline-block">
-        <span>
-          Entrez votre adresse...
+      <div class="flex justify-center">
+        <div class="mt-3 p-2 text-black border-3 rounded-4xl border-orange-500 inline-flex justify-center">
+        <span class="self-center ml-4 mr-4">
+          <InputText id="adress" type="text" placeholder="Entrez votre adresse..." class="w-full m-1 bronze-cesieats placeholder-cesieats"/>
         </span>
-        <span>
+          <span>
           <button class="bg-orange-400 button-cesieats">Rechercher</button>
         </span>
+        </div>
       </div>
 
     </div>
@@ -22,5 +25,11 @@
 </template>
 
 <style scoped>
+
+.placeholder-cesieats::placeholder {
+  color: #FA8231;
+  opacity: 75%;
+  font-weight: bold;
+}
 
 </style>
