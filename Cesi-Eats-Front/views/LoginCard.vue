06250c2924a526e714c79243ff5ed38113e9374c
Rayane <rayane.asaadi@viacesi.fr>
2025-03-26 10:24:30 +0100

    R

--- Cesi-Eats-Front/views/LoginCard.vue
+++ Cesi-Eats-Front/views/LoginCard.vue
@@ -1,15 +1,7 @@
 <template>
-  <div class="bg-surface-50 dark:bg-surface-950 px-6 py-20 md:px-12 lg:px-20">
-    <div class="bg-surface-0 dark:bg-surface-900 p-6 shadow rounded-border w-full lg:w-6/12 mx-auto">
-      <div class="text-center mb-8">
-        <svg class="mb-4 mx-auto fill-surface-600 dark:fill-surface-200 h-16" viewBox="0 0 30 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path
-              fill-rule="evenodd"
-              clip-rule="evenodd"
-              d="M20.7207 6.18211L14.9944 3.11148L3.46855 9.28678L0.579749 7.73444L14.9944 0L23.6242 4.62977L20.7207 6.18211ZM14.9996 12.3574L26.5182 6.1821L29.4216 7.73443L14.9996 15.4621L6.37724 10.8391L9.27337 9.28677L14.9996 12.3574ZM2.89613 16.572L0 15.0196V24.2656L14.4147 32V28.8953L2.89613 22.7132V16.572ZM11.5185 18.09L0 11.9147V8.81001L14.4147 16.5376V25.7904L11.5185 24.2312V18.09ZM24.2086 15.0194V11.9147L15.5788 16.5377V31.9998L18.475 30.4474V18.09L24.2086 15.0194ZM27.0969 22.7129V10.3623L30.0004 8.81V24.2653L21.3706 28.895V25.7904L27.0969 22.7129Z"
-          />
-        </svg>
-
+  <div class="-surface-50 dark:bg-surface-950 px-6 py-20 md:px-12 lg:px-20 ">
+    <div class=" m-1 rounded-lg border-2 border-orange-200 bg-orange-100bg -surface-0 dark:bg-surface-900 p-6 shadow rounded-border w-full lg:w-6/12 mx-auto ">
+      <div class="text-left mb-10 mt-4 ">
         <div class="text-surface-900 dark:text-surface-0 text-3xl font-medium mb-4">Se connecter</div>
         <span class="text-surface-600 dark:text-surface-200 font-medium leading-normal">Pas de compte ?</span>
         <a class="font-medium no-underline ml-2 text-primary cursor-pointer">Créer un compte</a>
@@ -17,16 +9,16 @@
 
       <div>
         <label for="email1" class="text-surface-900 dark:text-surface-0 font-medium mb-2 block">Adresse email</label>
-        <InputText id="email1" type="text" placeholder="Adresse email" class="w-full mb-4" />
+        <InputText id="email1" type="text" placeholder="Adresse email" class="w-full mb-4 m-1 " />
 
         <label for="password1" class="text-surface-900 dark:text-surface-0 font-medium mb-2 block">Mot de passe</label>
-        <InputText id="password1" type="password" placehoder="Mot de passe" class="w-full mb-4" />
+        <InputText id="password1" type="password" placeholder="Mot de passe" class="  w-full mb-4 m-1   " />
 
         <div class="flex items-center justify-between mb-12">
-          <a class="font-medium no-underline ml-2 text-primary text-right cursor-pointer">Forgot password?</a>
+          <a class="font-medium no-underline ml-2 text-primary text-right cursor-pointer border-orange-500">Forgot password?</a>
         </div>
 
-        <router-link label="Sign In" icon="pi pi-user !text-xl !leading-none" class="w-full nav-link ml-auto" to="/sign"> Sign In</router-link>
+        <router-link label="Sign In" icon="pi pi-user !text-xl !leading-none" class="nav-link w-full nav-link ml-auto" to="/sign"> Sign In</router-link>
       </div>
     </div>
   </div>
