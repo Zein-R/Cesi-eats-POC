2355df938d363f4d6dda2a435bce66056e967c76
aymen-elo <aymen.elouagouti@gmaile.com>
2025-03-26 14:02:03 +0100

    Add button update homeview

--- Cesi-Eats-Front/views/HomeView.vue
+++ Cesi-Eats-Front/views/HomeView.vue
@@ -8,8 +8,16 @@
     <div class="text-black w-full p-2">
       <h1 class="font-bold">Bienvenue sur <span class="bronze-cesieats">CESI Eats</span></h1>
       <div class="pl-1 opacity-50">Mangez ce que vous voulez (tout en restant en pyjama) !</div>
-    </div>
+      <div class="mt-3 p-2 text-black border-3 rounded-4xl border-orange-500 inline-block">
+        <span>
+          Entrez votre adresse...
+        </span>
+        <span>
+          <button class="bg-orange-400 button-cesieats">Rechercher</button>
+        </span>
+      </div>
 
+    </div>
   </div>
 </template>
 
