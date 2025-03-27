3e15172d8a142f77f90c957c430023a92ceb9784
aymen-elo <aymen.elouagouti@gmaile.com>
2025-03-27 10:24:16 +0100

    update login card

--- Cesi-Eats-Front/views/LoginCard.vue
+++ Cesi-Eats-Front/views/LoginCard.vue
@@ -7,31 +7,34 @@
 
       <div>
         <label for="email1" class="text-black dark:text-surface-0 font-medium block ml-1.5 mb-1">Adresse email</label>
-        <div class=" text-black border-3 rounded-2xl border-orange-500"><InputText id="email1" type="text" placeholder="xyz@example.com" class="w-full m-1 opacity-25 mb-2" /></div>
+        <div class=" text-black border-3 rounded-2xl border-orange-500"><InputText id="email1" type="text" placeholder="xyz@example.com" class="w-full m-1 opacity-25 mb-2 ml-3" /></div>
 
         <label for="password1" class="text-black dark:text-surface-0 font-medium block ml-1.5 pt-2 mb-1">Mot de passe</label>
-        <div class="text-black border-3 rounded-2xl border-orange-500"><InputText id="password1" type="password" placeholder="Mot de passe" class="w-full m-1 opacity-25 mb-2"/></div>
+        <div class="text-black border-3 rounded-2xl border-orange-500"><InputText id="password1" type="password" placeholder="Mot de passe" class="w-full m-1 opacity-25 mb-2  ml-3"/></div>
 
         <div class=" underline text-orange-500 justify-between mt-3 mb-6">
           <a class="font-weight no-underline ml-2 text-primary cursor-pointer mb-2">Mot de passe oublié ?</a>
         </div>
 
       </div>
-      <div class=" text-right mt-4">
-        <button class="  font-medium no-underline ml-2 text-primary cursor-pointer button-cesieats">Se connecter</button>
+      <div class="text-center mt-4">
+        <button class="font-medium no-underline ml-2 text-primary w-7/12 cursor-pointer button-cesieats">Se connecter</button>
       </div>
+
+      <div class="text-center mt-8">
+        <span class="text-black dark:text-surface-200 font-medium leading-normal opacity-50">Pas encore inscrit ?</span>
+      </div>
+
+      <div class="flex justify-center p-3">
+        <router-link to="/sign" class="text-white button-cesieats w-7/12 p-3 rounded-3xl text-center  router-link-active " > S'inscrire</router-link>
+      </div>
+
       <div class="flex justify-center">
         <div class="flex-row justify-between text-center">
-          <div>
-          <span class="text-black dark:text-surface-200 font-medium leading-normal">Pas encore inscrit ?</span>
-          </div>
 
-          <div class="flex justify-center p-3 mt-3">
-            <router-link to="/sign" class=" text-white bg-orange-400 w-2xs p-3 rounded-3xl text-center  router-link-active " > S'inscrire</router-link>
-          </div>
 
           <div class="mt-2">
-            <span>En vous connectant, vous confirmez accepter notre politique <br> de confidentialité, nos conditions d’utilisation et <br> notre politique en matière de cookies.</span>
+            <span class="text-black">En vous connectant, vous confirmez accepter notre politique <br> de confidentialité, nos conditions d’utilisation et <br> notre politique en matière de cookies.</span>
           </div>
         </div>
       </div>
