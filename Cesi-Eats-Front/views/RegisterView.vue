335feb9066513570c05fab7867842ec65d33ac85
Anass EZZOUHOUR <anass.ezzouhour@viacesi.fr>
2025-03-31 11:33:43 +0200

    anass register

--- Cesi-Eats-Front/views/RegisterView.vue
+++ Cesi-Eats-Front/views/RegisterView.vue
@@ -12,15 +12,15 @@
           <form class="space-y-4 md:space-y-6" action="#">
             <div>
               <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Adresse email</label>
-              <input type="email" name="email" id="email" class="bg-gray-50 border border-orange-500 border-3  text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="xyz@example.com" required="">
+              <input type="email" name="email" id="email" class="bg-gray-50 border border-orange-500 border-2  text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="xyz@example.com" required="">
             </div>
             <div>
               <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-              <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg border-orange-500 border-3 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
+              <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border-gray-300 text-gray-900 text-sm rounded-lg border-orange-500 border-2 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
             </div>
             <div>
               <label for="confirm-password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
-              <input type="confirm-password" name="confirm-password" id="confirm-password" placeholder="••••••••" class="bg-gray-50  border-orange-500 border-3  text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-orange-500 block w-full p-2.5 dark:bg-gray-700 dark:border-orange-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
+              <input type="password" name="confirm-password" id="confirm-password" placeholder="••••••••" class="bg-gray-50  border-orange-500 border-2  text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-orange-500 block w-full p-2.5 dark:bg-gray-700 dark:border-orange-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
             </div>
             <div class="flex items-start">
               <div class="flex items-center h-5">
@@ -30,9 +30,10 @@
                 <label for="terms" class="font-light text-gray-500 dark:text-gray-300">I accept the <a class="font-medium text-primary-600 hover:underline dark:text-primary-500" href="#">Terms and Conditions</a></label>
               </div>
             </div>
-            <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</button>
+            <button type="submit" class="w-full text-gray-500 bg-primary-600 hover:bg-amber-300 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800" >Create an account</button>
             <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-              Already have an account? <a href="#" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</a>
+              Already have an account?<router-link to="/Login" class="font-medium text-primary-600 hover:underline dark:text-primary-500  router-link-active " >Login here</router-link>
+
             </p>
           </form>
         </div>
